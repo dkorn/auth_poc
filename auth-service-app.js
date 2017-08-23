f24a9e414e92c6a521a7b21dca1151532115d1b5
dkorn <korndaniel1@gmail.com>
2017-08-23 17:55:47 +0300

    API Gateway POC with NGINX

--- auth-service-app.js
+++ auth-service-app.js
@@ -1,22 +1,106 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+let acl = require('acl');
 
 const app = express();
 app.use(bodyParser.json());
+acl = new acl(new acl.memoryBackend());
 
-app.post('/authenticate', (req, res) => {
-  const authenticated = req.body.authHeader === 'aaa';
-  if (!authenticated) {
-    console.log('not authenticated');
-    return res.status(403).end();
+const permissions = {GET: 'read', POST: 'create', PUT: 'update', DELETE: 'delete'};
+
+const mockUsersdDb = [
+  {userId: 'adminUser', password: 'secret', role: 'Admin', userApiToken: 'aaa'},
+  {userId: 'regularUser', password: 'secret', role: 'User', userApiToken: 'bbb'}
+];
+
+acl.addUserRoles('adminUser', 'Admin');
+acl.addUserRoles('regularUser', 'User');
+acl.allow([
+  {
+    roles: ['Admin'],
+    allows: [
+      { resources: 'incidents', permissions: '*' },
+      { resources: 'users', permissions: '*' }
+    ]
+  },
+  {
+    roles: ['User'],
+    allows: [
+      { resources: 'incidents', permissions: 'read' },
+      { resources: 'users', permissions: 'read' }
+    ]
   }
-  const authorized = req.body.neededPolicy.resource.id === '123';
-  if (!authorized) {
-    console.log('not authorized');
-    return res.status(403).end();
+]);
+
+function findUserByToken(token) {
+  return mockUsersdDb.find((user) => user.userApiToken === token);
+}
+
+function authenticateUser(req) {
+  const token = req.headers['authorization'];
+  return Promise.resolve(findUserByToken(token));
+}
+
+function authentication(req, res, next) {
+  console.log('Authenticating...');
+  authenticateUser(req).then((authenticated) => {
+    if (!authenticated) {
+      console.log('Not Authenticated!')
+      return res.status(401).end();
+    }
+    console.log('Authenticated by auth service!');
+    next()
+  });
+}
+
+function getUserRole(token) {
+  return findUserByToken(token).role;
+}
+
+function isAllowed(userId, resource, permission) {
+  return acl.isAllowed(userId, resource, permission);
+}
+
+function authorizeAction(req) {
+  const user = findUserByToken(req.headers['authorization']);
+  const method = req.headers['x-original-method'];
+  const resource = req.params.resource;
+  let permission;
+  if (req.params.id) {
+    const resourceId = req.params.id;
   }
-  console.log('zomg all ok!!1');
-  res.status(200).end();
-});
+  if (method == 'GET') {
+    permission = 'read';
+  } else {
+    permission = permissions[method];
+  }
+  return isAllowed(user.userId, resource, permission).then((authorized) => authorized);
+}
+
+function generateToken(req) {
+  const token = req.headers['authorization'];
+  const userRole = getUserRole(token);
+  const jwtToken = jwt.sign({ role: userRole }, 'shhhhh');
+  return jwtToken;
+}
+
+function authorization(req, res, next) {
+  console.log('Authorizing...');
+  authorizeAction(req).then((authorized) => {
+    if (!authorized) {
+      console.log('Not Authorized!')
+      return res.status(403).end();
+    }
+    console.log('Authorized by auth service!');
+    const jwtToken = generateToken(req); 
+    res.header('token', jwtToken);
+    res.status(200).end();
+  });
+}
+
+app.use('/api/:resource/:id/:subresource', authentication, authorization);
+app.use('/api/:resource/:id', authentication, authorization);
+app.use('/api/:resource', authentication, authorization);
 
 module.exports = app;
--- micro-service-app.js
+++ micro-service-app.js
@@ -5,52 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const authenticatedRoute = (req, res) => res.send('hello you authenticated you');
-
-const authMiddleWare = (neededPolicy) =>
-  (req, res, next) => {
-    const authHeader = req.headers['bp-wtf'];
-    const neededPolicyWithIDs = neededPolicy;
-    if (neededPolicy.resource) {
-      neededPolicyWithIDs.resource.id = req.params.id;
-    }
-    console.log('in auth middleware', authHeader, neededPolicyWithIDs);
-    return request.post({
-      uri: 'http://localhost:1448/authenticate',
-      json: true,
-      resolveWithFullResponse: true,
-      body: {
-        authHeader,
-        neededPolicy: neededPolicyWithIDs
-      }
-    })
-    .then((response) => {
-      if (response.statusCode === 200) {
-        console.log('authenticated by auth service');
-        return next();
-      }
-      console.log('not authenticated', response.statusCode);
-      return res.status(403).send('Not authenticated');
-    })
-    .catch((err) => {
-      console.log('error from auth service', err.statusCode);
-      res.status(err.statusCode).send('Auth error');
-    });
-  };
-
-app.get('/hello', (req, res, next) => {
-  console.log('in middleware');
+app.get('/api/*', (req, res, next) => {
+  console.log('in microservice');
+  console.log(req.headers);
   next();
 }, (req, res) => res.send('hello'));
 
-app.get('/environment/:id', authMiddleWare({
-        resource: {
-          type: 'environment'
-        },
-        permissions: [
-          'environment@read'
-        ]
-      }
-), authenticatedRoute);
+app.delete('/api/*', (req, res, next) => {
+  console.log('in microservice');
+  console.log(req.headers);
+  next();
+}, (req, res) => res.send('hello'));
 
 module.exports = app;
