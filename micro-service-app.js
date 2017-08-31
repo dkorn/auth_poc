d4c26702fddc7148990f1b9dad86b04abc7b51f0
dkorn <korndaniel1@gmail.com>
2017-08-31 14:19:50 +0300

    Return user roles and resources ids in response
    The response includes also the user ID.
    The resources IDs are returned in the case of `GET` request for a
    collection of resources.

--- micro-service-app.js
+++ micro-service-app.js
@@ -7,17 +7,33 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/api/*', (req, res, next) => {
-  console.log('in microservice');
-  const token = jwt.verify(req.headers['token'], 'shhhhh');
-  console.log('jwt token:', token);
+  console.log('in microservice - get');
+  const jwtToken = jwt.verify(req.headers['token'], 'shhhhh');
+  console.log('jwt token:', jwtToken);
+  res.locals.token = jwtToken;
   next();
-}, (req, res) => res.send('hello'));
+}, (req, res) => {
+  let token = res.locals.token;
+  delete token.user.password;
+  delete token.user.userApiToken;
+  delete token.iat;
+  res.send(token);
+  }
+);
 
 app.delete('/api/*', (req, res, next) => {
-  console.log('in microservice');
-  const token = jwt.verify(req.headers['token'], 'shhhhh');
-  console.log('jwt token:', token);
+  console.log('in microservice - delete');
+  const jwtToken = jwt.verify(req.headers['token'], 'shhhhh');
+  console.log('jwt token:', jwtToken);
+  res.locals.token = jwtToken;
   next();
-}, (req, res) => res.send('hello'));
+}, (req, res) => {
+  let token = res.locals.token;
+  delete token.user.password;
+  delete token.user.userApiToken;
+  delete token.iat;
+  res.send(token);
+  }
+);
 
 module.exports = app;
